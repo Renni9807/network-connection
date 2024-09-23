2afe70dbf8c1c2d557d37992f2217aea41f19271
Renni9807 <117729123+Renni9807@users.noreply.github.com>
2024-09-23 22:55:14 +0800

    trivial edit

--- fabricNetwork.js
+++ fabricNetwork.js
@@ -4,22 +4,22 @@ const path = require("path");
 const fs = require("fs");
 
 async function connectToNetwork() {
-  // 연결 프로파일 로드
+  // load the connection profile
   const ccpPath = path.resolve(__dirname, "connection.json");
   const ccp = JSON.parse(fs.readFileSync(ccpPath, "utf8"));
 
-  // 지갑 로드
+  // load wallet
   const walletPath = path.join(process.cwd(), "wallet");
   const wallet = await Wallets.newFileSystemWallet(walletPath);
 
-  // 사용자 확인
+  // verify AppUser
   const identity = await wallet.get("AppUser");
   if (!identity) {
     console.log("Cannot find the wallet, register first");
     return;
   }
 
-  // 게이트웨이 연결
+  // Gateway connection
   const gateway = new Gateway();
   await gateway.connect(ccp, {
     wallet,
@@ -27,7 +27,7 @@ async function connectToNetwork() {
     discovery: { enabled: true, asLocalhost: true },
   });
 
-  // 네트워크 및 스마트 컨트랙트 접근
+  // network and contract access
   const network = await gateway.getNetwork("mychannel");
   const contract = network.getContract("mychaincode");
 
